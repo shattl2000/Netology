1e36e780fbb10a0631ddcfd189d18505ea066e82
Shattl <shattl2000@yandex.ru>
2019-07-05 21:01:02 +0300

    Changed file from lesson 8

--- Python_course/Part 1/Lesson 8/cook-book.py
+++ Python_course/Part 1/Lesson 8/cook-book.py
@@ -1,26 +1,28 @@
-with open("recipes.txt", encoding="utf-8") as f:
-    cook_book = {}
-    line = f.readline()
-    while line:
-        name_dish = line.strip()
-        count_ingredients = int(f.readline().strip())
-        ingredient_list = []
-        for _ in range(count_ingredients):
-            ingredient_name, quantity, measure = f.readline().strip().split(' | ')
-            ingredient_dict = {}
-            ingredient_dict['ingridient_name'] = ingredient_name
-            ingredient_dict['quantity'] = int(quantity)
-            ingredient_dict['measure'] = measure
-            ingredient_list.append(ingredient_dict)
-        cook_book[name_dish] = ingredient_list
-        f.readline()
+def cook_book():
+    with open("recipes.txt", encoding="utf-8") as f:
+        cook_book = {}
         line = f.readline()
+        while line:
+            name_dish = line.strip()
+            count_ingredients = int(f.readline().strip())
+            ingredient_list = []
+            for _ in range(count_ingredients):
+                ingredient_name, quantity, measure = f.readline().strip().split(' | ')
+                ingredient_dict = {}
+                ingredient_dict['ingridient_name'] = ingredient_name
+                ingredient_dict['quantity'] = int(quantity)
+                ingredient_dict['measure'] = measure
+                ingredient_list.append(ingredient_dict)
+            cook_book[name_dish] = ingredient_list
+            f.readline()
+            line = f.readline()
+    return cook_book
 
-print(cook_book)
+print(cook_book())
 
 
 def get_shop_list_by_dishes(list_of_dishes, person_count):
-    recipes = cook_book
+    recipes = cook_book()
     shop_dict = {}
     for key, value in recipes.items():
         if key in list_of_dishes:
