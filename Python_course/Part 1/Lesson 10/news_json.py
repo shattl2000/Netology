9a67cfb566b1406c0a9beab49dbc96fda34605b2
Shattl <shattl2000@yandex.ru>
2019-07-07 15:15:29 +0300

    Add task json and xml from lesson 10

---
+++ Python_course/Part 1/Lesson 10/news_json.py
@@ -0,0 +1,33 @@
+import json
+from pprint import pprint
+
+
+def count_10_word_in_json():
+    from collections import Counter
+    with open("newsafr.json", encoding="utf-8") as datafile:
+        json_data = json.load(datafile)
+        json_items = json_data["rss"]["channel"]["items"]
+
+        descriptions = []
+
+        for i in json_items:
+            descriptions.append(i["description"].split())
+
+        format_description = []
+
+        for elem in sum(descriptions, []):
+            if len(elem) > 6:
+                format_description.append(elem)
+
+        def sortByLength(inputStr):
+            return len(inputStr)
+
+        format_description.sort(key=sortByLength, reverse=True)
+
+        Counter = Counter(format_description)
+        words = Counter.most_common(10)
+        for word in words:
+            pprint(f"Слово: '{word[0]}' встречается: {word[1]} раз")
+
+
+count_10_word_in_json()
---
+++ Python_course/Part 1/Lesson 10/news_xml.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as ET
+
+
+def count_10_word_in_xml():
+    from collections import Counter
+    tree = ET.parse("newsafr.xml")
+    descriptions = []
+    root = tree.getroot()
+    xml_items = root.findall("channel/item")
+
+    for item in xml_items:
+        description = item.find("description")
+        descriptions += description.text.split()
+
+    format_description = []
+    for word in descriptions:
+        if len(word) > 6:
+            format_description.append(word)
+
+    def sortByLength(inputStr):
+        return len(inputStr)
+
+    format_description.sort(key=sortByLength, reverse=True)
+
+    Counter = Counter(format_description)
+    words = Counter.most_common(10)
+    for word in words:
+        print(f"Слово: '{word[0]}' встречается: {word[1]} раз")
+
+
+count_10_word_in_xml()
