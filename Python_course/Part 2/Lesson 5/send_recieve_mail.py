5bf1ccb7cfbe17f8910e385f0151eb83f10a85c5
Александр Кузьмин <shattl2000@MacBook-Pro-Aleksandr.local>
2019-08-19 22:41:31 +0300

    Refactor code

--- Python_course/Part 2/Lesson 5/send_recieve_mail.py
+++ Python_course/Part 2/Lesson 5/send_recieve_mail.py
@@ -8,36 +8,29 @@ from email.mime.multipart import MIMEMultipart
 class SendEmail:
 
     def __init__(self):
-        self.SMTP_SERVER = 'smtp.yandex.ru'
-        self.IMAP_SERVER = 'imap.yandex.ru'
-        self.email_login = 'my_mail_adress@yandex.ru'
-        self.password = 'qwerty'
-        self.subject = 'Mail from python application'
-        self.recipients = ['vasya@email.com', 'petya@email.com']
-        self.message = 'Hello my friend!'
-        self.header = None
+        pass
 
-    def send_message(self):
-        msg = MIMEMultipart()
-        msg['From'] = self.email_login
-        msg['To'] = ', '.join(self.recipients)
-        msg['Subject'] = self.subject
-        msg.attach(MIMEText(self.message))
-        server = smtplib.SMTP(self.SMTP_SERVER, 587)
-        server.ehlo()
+    def send_message(self, email_login, recipients, subject, message,
+                     password, smtp_server='smtp.yandex.ru'):
+        server = smtplib.SMTP(smtp_server, 587)
         server.starttls()
-        server.ehlo()
-        server.login(self.email_login, self.password)
-        server.sendmail(self.email_login, self.recipients, msg.as_string())
+        server.login(email_login, password)
+        msg = MIMEMultipart()
+        msg['From'] = email_login
+        msg['To'] = ', '.join(recipients)
+        msg['Subject'] = subject
+        msg.attach(MIMEText(message))
+        server.sendmail(email_login, recipients, msg.as_string())
         server.quit()
+        print('Email send')
 
-    def recieve(self):
-        mail = imaplib.IMAP4_SSL(self.IMAP_SERVER)
-        mail.login(self.email_login, self.password)
+    def recieve(self, email_login, password,
+                imap_server='imap.yandex.ru', header=None):
+        mail = imaplib.IMAP4_SSL(imap_server)
+        mail.login(email_login, password)
         mail.list()
         mail.select("inbox")
-        criterion = '(HEADER Subject "%s")' % self.header if self.header \
-            else 'ALL'
+        criterion = '(HEADER Subject "%s")' % header if header else 'ALL'
         result, data = mail.uid('search', None, criterion)
         assert data[0], 'There are no letters with current header'
         latest_email_uid = data[0].split()[-1]
@@ -47,7 +40,49 @@ class SendEmail:
         mail.logout()
 
 
+def main():
+    while True:
+        sender_email = input('Введите ваш e-mail отправителя: ')
+        if '@' and '.' in sender_email:
+            password = input('Введите пароль: ')
+            while True:
+                print('Для отправки сообщения введите "send", '
+                      'для чтения по заголовку введите "recieve",'
+                      ' для выхода введите "exit"')
+                to_do = input('Введите команду:')
+                if to_do == 'send':
+                    recipients_list = list(
+                        map(str, input('Введите получателей: ').split(', '))
+                    )
+                    subject = str(input('Введите тему сообщения: '))
+                    message = str(input('Введите сообщение:'))
+                    send_email = SendEmail()
+                    send_email.send_message(
+                        email_login=sender_email,
+                        recipients=recipients_list,
+                        subject=subject,
+                        message=message,
+                        password=password,
+                    )
+                    break
+                elif to_do == 'recieve':
+                    header = input('Введите заголовок письма')
+                    send_email = SendEmail()
+                    send_email.recieve(
+                        email_login=sender_email,
+                        password=password,
+                        header=header,
+                    )
+                    break
+                elif to_do == 'exit':
+                    print('Выход')
+                    break
+                else:
+                    print('Команда введена неверно!')
+            break
+        else:
+            print('Вы ввели некорректный e-mail, попробуйте еще раз! : ')
+
+
 if __name__ == '__main__':
-    mail = SendEmail()
-    mail.send_message()
-    mail.recieve()
+    main()
